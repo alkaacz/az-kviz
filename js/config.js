c4c8bc348a636ee70371b52ca0b5c64315f95bd3
alkaacz <alkaacz@users.noreply.github.com>
2026-05-30 22:29:15 +0200

    api url

--- js/config.js
+++ js/config.js
@@ -9,7 +9,8 @@ export const CONFIG = {
  // aud            "279978880894-ibqderbn40hoq83mg0pbdld7lvcgvssv.apps.googleusercontent.com"
   // URL Google Apps Script Web App (po deploymentu z script.google.com)
   API_URL: // 'https://script.google.com/macros/s/AKfycbziHhilErprUmNdc_W4_VGhzt3Ikl13iQtYJczJzP_CdUJj1mtZmVukmGzZtewDKvzwuA/exec',
-          'https://script.google.com/macros/s/AKfycbwYoDHEzowGRfbKW0_SwMwQA8FOmfDZZ1LaJH-GFK6sVICJ2-IybGWXrtxnB8yJh3EkVw/exec', 
+        //  'https://script.google.com/macros/s/AKfycbwYoDHEzowGRfbKW0_SwMwQA8FOmfDZZ1LaJH-GFK6sVICJ2-IybGWXrtxnB8yJh3EkVw/exec', 
+          'https://script.google.com/macros/s/AKfycbyZMMayZhvACvHUH00P44m6ZXbn1nkHNCBUQ546CTMiXQCZSr79R_o0aoXVqcxBDbzESg/exec',
 
   // Spreadsheet ID z URL: docs.google.com/spreadsheets/d/TOTO-JE-ID/edit
   SPREADSHEET_ID: '1KrqJ5xrQ5WfVX9OY8YG8CuDvWelqUf_CYqaRW5p8psE',
